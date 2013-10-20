32b051fb776654a343d0296b5728d85da3b72f63
Zhiyuan Wang <zywang@u.northwestern.edu>
2013-10-19 21:24:28 -0500

    init

--- code/air_freight.c
+++ code/air_freight.c
@@ -3,7 +3,17 @@
 const int r = 1;
 const int c = 10;
 
-int main()
+int* schedule(int* week, int n)
 {
+  int i;
+  for(i=0;i<n;i++)
+    printf("%d\n", week[i]);
+}
+
+int main()
+{ 
+  int n = 10;
+  int test[10]= {11, 9, 9, 12, 12, 12, 12, 9, 9, 11};
+  schedule(test, n);
   //printf("%d\t%d\n", r, c);
 }
